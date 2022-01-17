a279b0f43367cebee36e1460210907a3fa8367e2
misaraadel <misaraadel@gmail.com>
2022-01-17 19:10:31 +0200

    add counter shape

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,136 @@
-const startingMinutes = 2 ;
-let time = startingMinutes * 60 ;
-const ParentEl = document.querySelector('.count-box');
-const countDownEl = document.getElementById('countDown');
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
 
-function UpdateTimeDown(startingMinutes) {
-    interval = setInterval(function() {
-        var minutes = Math.floor(time / 60);
-        var second = time % 60;
+const COLOR_CODES = {
+  info: {
+    color: "green"
+  },
+  warning: {
+    color: "purple",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "blue",
+    threshold: ALERT_THRESHOLD
+  }
+};
 
-        countDownEl.innerHTML = minutes + ":" + second; 
+const TIME_LIMIT = 20;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
 
-        if(second >=0 ){
-            ParentEl.classList.add('sucess');
-            time-- ; 
-        }else{
-            clearInterval(interval);
-            countDownEl.innerHTML = `00 : 0`;
-            ParentEl.classList.remove('sucess').add('blue');
-        }
-    } , 1000)
+document.getElementById("app").innerHTML = `
+<div class="count-box base-timer green" id="baseColor">
+<div class="icon">
+    <img src="assets/images/icon.svg" alt="">
+</div>
+  <svg class="base-timer__svg" viewBox="0 0 100 100" width="150" height="150" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+  </svg>
+  <span id="base-timer-label" class="base-timer__label count">${formatTime(
+    timeLeft
+  )}</span>
+
+<h4 class="name">
+    eman
+</h4>
+</div>
+`;
+
+startTimer();
+
+function onTimesUp() {
+  clearInterval(timerInterval);
 }
 
-UpdateTimeDown(startingMinutes)
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.getElementById("base-timer-label").innerHTML = formatTime(
+      timeLeft
+    );
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
 
+    if (timeLeft === 0) {
+      onTimesUp();
+    }
+  }, 1000);
+}
 
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
 
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${minutes}:${seconds}`;
+}
+
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES;
+  if (timeLeft <= alert.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color);
+
+    document
+        .getElementById("baseColor")
+        .classList.remove(warning.color);
+    document
+
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color);
+
+    document
+      .getElementById("baseColor")
+      .classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color);
+
+    document
+      .getElementById("baseColor")
+      .classList.remove(info.color);
+    document
+      .getElementById("baseColor")
+      .classList.add(warning.color);
+  }
+}
+
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
